967fdeb57ade633847569f88972843ba5879dc77
= <=>
2017-03-24 15:30:09 +0530

    First Commit

---
+++ Login/src/p1/LoginServlet.java
@@ -0,0 +1,63 @@
+package p1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("check");
+		PrintWriter out= response.getWriter();
+		String str1=request.getParameter("txtuser");
+		String str2=request.getParameter("txtpass");
+		ConnectionCls c1= new ConnectionCls(str1,str2);
+		try {
+			int i=c1.checkData();
+			if(i>0)
+			{
+				out.println("Login Succesful");
+			}
+			else
+			{
+				out.print("Re-enter proper data");
+			}
+		} 
+		catch (ClassNotFoundException | SQLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
