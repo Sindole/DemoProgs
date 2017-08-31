1660adb9418d48869c76f2b306f79e1e106c396a
= <=>
2017-08-31 23:50:17 +0530

    Second Testing

---
+++ LoginApp/src/p1/LoginController.java
@@ -0,0 +1,66 @@
+package p1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/LoginController")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		String str1=request.getParameter("txtUser");
+		String str2=request.getParameter("txtPass");
+		LoginDAO lg=new LoginDAO(str1,str2);
+		try {
+			int x=lg.checkData();
+			if(x>0)
+			{
+				out.println("success");
+				RequestDispatcher rd=request.getRequestDispatcher("Success.jsp");
+				rd.include(request,response);
+			}
+			else
+			{
+				out.println("Failed");
+				RequestDispatcher rd=request.getRequestDispatcher("Fail.jsp");
+				rd.include(request,response);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
